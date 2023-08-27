c480f9289f2bd95b5fc2478e58cb0d4b4bfa5bac
alexvasilkovski <alexvasilkovski@outlook.com>
2023-08-27 21:56:38 +0100

    create modules

---
+++ hosts/nixos/home.nix
@@ -0,0 +1,63 @@
+{
+  pkgs,
+  ...
+}: {
+  home.stateVersion = "23.11";
+
+  # Configure programs
+  programs = {
+    nushell = {
+      enable = true;
+      shellAliases = {
+        switch = "sudo nixos-rebuild switch --impure";
+      };
+      environmentVariables = {
+        EDITOR = "hx";
+        VISUAL = "bat";
+      };
+      configFile.text = ''
+        $env.config = {
+          show_banner: false,
+        }
+      '';
+    };
+    git = {
+      enable = true;
+      delta.enable = true;
+      extraConfig = {
+        "init" = {
+          defaultBranch = "main";
+        };
+        "user" = {
+          name = "Niko";
+          email = "97130632+Nyabinary@users.noreply.github.com";
+        };
+        "push" = {
+          autoSetupRemote = true;
+        };
+        "credential" = {
+          helper = "store";
+        };
+      };
+    };
+    helix = {
+      enable = true;
+      settings = {
+        theme = "catppuccin_mocha";
+        editor = {
+          line-number = "relative";
+          lsp.display-messages = true;
+        };
+      };
+    };
+    # vscode = {
+    #   enable = true;
+    # };
+    autojump = {
+      enable = true;
+    };
+    bat = {
+      enable = true;
+    };
+  };
+};
